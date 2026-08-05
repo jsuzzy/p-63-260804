d653cd5031f71cdd644665fc25be4e332a104de4
jeongsuji <sujijeong11@gmail.com>
2026-08-05 10:53:17 +0900

    jpa 메서드

--- src/test/java/com/back/p63260804/QuestionRepositoryTest.java
+++ src/test/java/com/back/p63260804/QuestionRepositoryTest.java
@@ -41,4 +41,27 @@ class QuestionRepositoryTest {
         }
     }
 
+    @Test
+    @DisplayName("findBySubject")
+    void t3() {
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?").get();
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    @DisplayName("findBySubjectAndContent")
+    void t4() {
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.").get();
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    @DisplayName("findBySubjectLike")
+    void t5() {
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
 }
